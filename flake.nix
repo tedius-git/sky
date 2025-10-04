64522005c870690312157e5e3551b3717d18a232
tedius-git <diegominasantos@gmail.com>
2025-10-04 09:54:06 -0600

    chore:all nix in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,29 @@
   description = "Development Shell";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = import ./shell.nix { inherit pkgs; };
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.${system}.default = pkgs.mkShell {
+          shellHook = ''
+            echo "Development environment loaded"
+          '';
+          packages = with pkgs; [
+            gleam
+            erlang_27
+            nodejs_22
+            rebar3
+            inotify-tools
+          ];
+        };
+      }
+    );
 }
